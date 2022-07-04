8840d6a21f2c1902aa93aa89b0002941311f7b4c
Alexander Chernyshev <33011221+AlexanderChernyshev@users.noreply.github.com>
2022-07-04 14:28:33 -0400

    refactored code to move training into a tileset class

--- waveformgen.js
+++ waveformgen.js
@@ -9,24 +9,69 @@ class Tile {
     }
 }
 
-const tiles = {
-    leftbigstep: new Tile("leftbigstep", "leftbigsteptile.png"),
-    rightbigstep: new Tile("rightbigstep", "rightbigsteptile.png"),
-    block: new Tile("block", "blocktile.png"),
-    clouds: new Tile("clouds", "cloudstile.png"),
-    column: new Tile("column", "columntile.png"),
-    ornatewindow: new Tile("ornatewindow", "ornatewindowtile.png"),
-    potslab: new Tile("potslab", "potslabtile.png"),
-    sky: new Tile("sky", "skytile.png"),
-    slab: new Tile("slab", "slabtile.png"),
-    leftslope: new Tile("leftslope", "leftslopetile.png"),
-    rightslope: new Tile("rightslope", "rightslopetile.png"),
-    leftsmallslope: new Tile("leftsmallslope", "leftsmallslopetile.png"),
-    rightsmallslope: new Tile("rightsmallslope", "rightsmallslopetile.png"),
-    leftstairs: new Tile("leftstairs", "leftstairstile.png"),
-    rightstairs: new Tile("rightstairs", "rightstairstile.png"),
-    windows: new Tile("windows", "windowstile.png"),
-};
+class Tileset {
+    constructor() {
+        this.tiles = {}
+    }
+
+    add(tile) {
+        this.tiles[tile.id] = tile;
+    }
+
+    train(map) {
+
+        for (let rowIndex = 0; rowIndex < map.length; rowIndex++) {
+
+            for (let cellIndex = 0; cellIndex < map[rowIndex].length; cellIndex++) {
+                let currentId = map[rowIndex][cellIndex];
+                let currenttile = this.tiles[currentId];
+
+                if (rowIndex > 0) {
+                    let northId = map[rowIndex - 1][cellIndex];
+                    currenttile.north.push(northId);
+                }
+
+                if (cellIndex > 0) {
+                    let westId = map[rowIndex][cellIndex - 1];
+                    currenttile.west.push(westId);
+                }
+
+                if (cellIndex < map[rowIndex].length - 1) {
+                    let eastId = map[rowIndex][cellIndex + 1];
+                    currenttile.east.push(eastId);
+                }
+
+                if (rowIndex < map.length - 1) {
+                    let southId = map[rowIndex + 1][cellIndex];
+                    currenttile.south.push(southId);
+                }
+
+            }
+
+        }
+
+    }
+
+}
+
+const tileset = new Tileset();
+
+tileset.add(new Tile("leftbigstep", "leftbigsteptile.png"));
+tileset.add(new Tile("rightbigstep", "rightbigsteptile.png"));
+tileset.add(new Tile("block", "blocktile.png"));
+tileset.add(new Tile("clouds", "cloudstile.png"));
+tileset.add(new Tile("column", "columntile.png"));
+tileset.add(new Tile("ornatewindow", "ornatewindowtile.png"));
+tileset.add(new Tile("potslab", "potslabtile.png"));
+tileset.add(new Tile("sky", "skytile.png"));
+tileset.add(new Tile("slab", "slabtile.png"));
+tileset.add(new Tile("leftslope", "leftslopetile.png"));
+tileset.add(new Tile("rightslope", "rightslopetile.png"));
+tileset.add(new Tile("leftsmallslope", "leftsmallslopetile.png"));
+tileset.add(new Tile("rightsmallslope", "rightsmallslopetile.png"));
+tileset.add(new Tile("leftstairs", "leftstairstile.png"));
+tileset.add(new Tile("rightstairs", "rightstairstile.png"));
+tileset.add(new Tile("windows", "windowstile.png"));
 
 let example_map = [
     ["sky", "sky", "sky", "sky", "sky", "sky", "clouds", "sky", "sky", "sky", "sky"],
@@ -44,50 +89,16 @@ function generateMap(map) {
         for (let cellIndex = 0; cellIndex < map[rowIndex].length; cellIndex++) {
             const imageEl = document.createElement("img");
             const tileId = map[rowIndex][cellIndex];
-            imageEl.setAttribute("src", `tiles/${tiles[tileId].image}`);
+            imageEl.setAttribute("src", `tiles/${tileset.tiles[tileId].image}`);
             rowEl.appendChild(imageEl);
         }
         output.appendChild(rowEl);
     }
 }
 
-function trainTileSet(map, tiles) {
-
-    for (let rowIndex = 0; rowIndex < map.length; rowIndex++) {
-
-        for (let cellIndex = 0; cellIndex < map[rowIndex].length; cellIndex++) {
-            let currentId = map[rowIndex][cellIndex];
-            let currenttile = tiles[currentId];
-
-            if (rowIndex > 0) {
-                let northId = map[rowIndex - 1][cellIndex];
-                currenttile.north.push(northId);
-            }
-
-            if (cellIndex > 0) {
-                let westId = map[rowIndex][cellIndex - 1];
-                currenttile.west.push(westId);
-            }
-
-            if (cellIndex < map[rowIndex].length - 1) {
-                let eastId = map[rowIndex][cellIndex + 1];
-                currenttile.east.push(eastId);
-            }
-
-            if (rowIndex < map.length - 1) {
-                let southId = map[rowIndex + 1][cellIndex];
-                currenttile.south.push(southId);
-            }
-
-        }
-
-    }
-
-}
 
 
-
-trainTileSet(example_map, tiles);
+tileset.train(example_map);
 generateMap(example_map);
 
-console.log(tiles);
+console.log(tileset);
